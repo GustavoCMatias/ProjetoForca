076e5393d7b4e73a3b9a5401384bfc1c47c316d3
GustavoCMatias <gustavocmatias@gmail.com>
2022-11-25 13:43:27 -0300

    botoes desabilitados apos o clique

--- src/components/Letra.js
+++ src/components/Letra.js
@@ -1,6 +1,16 @@
+import React from "react"
+
 export default function Letra({letra, cliqueLetra}){
+    const [desabilitado, setDesabilitado] = React.useState(false)
+    function cliqueLetraCompleto(letra){
+        cliqueLetra(letra)
+        setDesabilitado(true)
+    }
+
     return(
-        <button className="letra botao" onClick={()=>cliqueLetra(letra)}>
+        <button className="letra botao" 
+        onClick={()=>cliqueLetraCompleto(letra)} 
+        disabled={desabilitado ? true : false}>
             <p>{letra.toUpperCase()}</p>
         </button>
     )
